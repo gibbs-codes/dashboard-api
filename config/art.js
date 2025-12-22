35c616d828b42d69875dc0e7846792ac0b18245d
jgibbswork <james.gibbs@me.com>
2025-12-22 13:01:57 -0800

    lets see

---
+++ config/art.js
@@ -0,0 +1,31 @@
+/**
+ * Art source configuration and rotation settings
+ */
+module.exports = {
+  poolSize: 12,
+  poolTtlSeconds: 3600,
+  retryDelayMs: 500,
+  rotationIntervals: {
+    portrait: 300, // 5 minutes
+    landscape: 420, // 7 minutes
+    tv: 360 // 6 minutes
+  },
+  sources: {
+    artic: {
+      enabled: true,
+      weight: 40,
+      styles: ['Cubism', 'Expressionism', 'Surrealism', 'Abstract', 'Minimalism', 'Constructivism', 'Symbolism', 'Suprematism', 'Bauhaus']
+    },
+    met: {
+      enabled: true,
+      weight: 40,
+      hasImages: true,
+      departments: [11, 21, 26, 30] // European Paintings, The American Wing, Drawings/Prints, Photographs
+    },
+    cleveland: {
+      enabled: true,
+      weight: 20,
+      type: 'Painting'
+    }
+  }
+};
